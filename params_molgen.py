7be09179d141340b2d55ff1e2a65a9903bef071b
C-user for moeinzad <Cmoeinza@phaedra.molgen.mpg.de>
2020-02-17 10:38:34 +0100

    Separate parameter files added for molgen and public github

---
+++ params_molgen.py
@@ -0,0 +1,14 @@
+RNAseqbams = ['L18098Aligned.sortedByCoord.out.bam', 'L18099Aligned.sortedByCoord.out.bam',
+                  'L18100Aligned.sortedByCoord.out.bam']
+path = '/confidential/FamilyR13_data/moeinzadeh/ossifican_progressiva/'
+vcf_file = '/confidential/FamilyR13_data/moeinzadeh/ossifican_progressiva/brusco_uwcmg_eho_1.HF.final.phased.vcf.gz'
+individual_name = '176126'
+interval_string = 'X:1-155270560'
+interval_string = 'X:153070560-155270560'
+open_left_region = 0
+open_right_region = 0
+readmefile = 'readme_molgen.txt'
+
+
+
+
